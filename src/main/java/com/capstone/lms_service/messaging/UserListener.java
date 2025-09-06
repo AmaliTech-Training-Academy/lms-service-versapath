7cdf466471cf55dcbd23cc65bff7fc24aebcaa68
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-06 10:40:57 +0200

    test: comment out maven deployed repository

--- src/main/java/com/capstone/lms_service/messaging/UserListener.java
+++ src/main/java/com/capstone/lms_service/messaging/UserListener.java
@@ -1,14 +1,18 @@
 package com.capstone.lms_service.messaging;
 
-import org.common.event.CreateUserEvent;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserListener {
+    private static final Logger logger = LoggerFactory.getLogger(UserListener.class);
     @RabbitListener(queues = "versapath.user.created")
-    public void handleMoodleUserCreation(CreateUserEvent userEvent) {
+    public void handleMoodleUserCreation(String userEvent) {
         // TODO: create user to moodle
-        System.out.println("Start creating user: " + userEvent.getEmail());
+        logger.info("Start creating user: {}", userEvent);
     }
 }
