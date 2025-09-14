af0208d939b9bad0b65d9a69d378ac2403ff8bb9
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-14 18:53:52 +0200

    feat: add security to authenticate users

---
+++ src/main/java/com/capstone/lms_service/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.capstone.lms_service.config;
+
+import com.capstone.lms_service.filter.UserContextFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserContextFilter userContextFilter;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/actuator/health"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.setContentType("application/json");
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.getWriter().write(
+                                    "{"
+                                    + "\"success\":false,"
+                                    + "\"message\":\"JWT token is missing or invalid\","
+                                    + "\"data\":null,"
+                                    + "\"errors\":null"
+                                    + "}"
+                            );
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setContentType("application/json");
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                            response.getWriter().write(
+                                    "{"
+                                    + "\"success\":false,"
+                                    + "\"message\":\"You don't have permission to access this resource\","
+                                    + "\"data\":null,"
+                                    + "\"errors\":null"
+                                    + "}"
+
+                            );
+                        })
+                )
+                .addFilterBefore(userContextFilter, BasicAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+}
