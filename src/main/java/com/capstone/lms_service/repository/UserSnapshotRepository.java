046af107f6a56f9e4a90f67bfdc064f4d7b28728
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-10-11 10:39:50 +0200

    feat: find user by Moodle webservice token

--- src/main/java/com/capstone/lms_service/repository/UserSnapshotRepository.java
+++ src/main/java/com/capstone/lms_service/repository/UserSnapshotRepository.java
@@ -4,8 +4,11 @@ import com.capstone.lms_service.model.UserSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserSnapshotRepository extends JpaRepository<UserSnapshot, UUID> {
+    Optional<UserSnapshot> findUserSnapshotByMoodleUserToken(String learnerToken);
+
 }
